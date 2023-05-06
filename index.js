3198045a95b7aeab11361d52fde03a1f8abc4ac6
Arman7052 <99arman7052@gmail.com>
2023-05-07 00:52:28 +0600

    Adding users

--- index.js
+++ index.js
@@ -2,8 +2,36 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
-app.use(cors());
 
+
+
+const users = [
+    {
+        id: 1,
+        name: "John Doe",
+        email: "john.doe@example.com"
+    },
+    {
+        id: 2,
+        name: "Jane Smith",
+        email: "jane.smith@example.com"
+    },
+    {
+        id: 3,
+        name: "Bob Johnson",
+        email: "bob.johnson@example.com"
+    },
+    {
+        id: 4,
+        name: "Samantha Lee",
+        email: "samantha.lee@example.com"
+    },
+    {
+        id: 5,
+        name: "Tom Wilson",
+        email: "tom.wilson@example.com"
+    }
+]
 
 
 
@@ -11,6 +39,7 @@ app.get('/users', (req,res) => {
     res.send(users)
 })
 
+app.use(cors());
 
 
 
